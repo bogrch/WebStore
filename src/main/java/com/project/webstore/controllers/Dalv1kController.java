bd48917aefa47409ae77a727fb1195cc87facc9a
DaLv1ko <vlad1602dubno2552dalv@gmail.com>
2021-02-11 22:48:17 +0200

    created hello controller

---
+++ src/main/java/com/project/webstore/controllers/Dalv1kController.java
@@ -0,0 +1,13 @@
+package com.project.webstore.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Dalv1kController {
+
+    @RequestMapping("/hello")
+    public String dalv1k(){
+        return "pizdec";
+    }
+}
